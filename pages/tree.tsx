b9aceb54e72d7b89f9dee3611ade176c1f3c4855
Justin Lee <justinlee0777@gmail.com>
2025-06-30 16:05:10 -0400

    Initial commit

---
+++ pages/tree.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+
+import { Tree } from '../src/shared/Tree';
+
+const root = ReactDOM.createRoot(document.body);
+root.render(<Tree />);
---
+++ src/shared/Tree.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from 'react';
+
+export function Tree(): JSX.Element {
+  return <div>Hello tree-world!</div>;
+}
